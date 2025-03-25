8585d4a7af55085f3f7d9632ac218ebffbc4944b
MSAbishek <subbuabishek@gmail.com>
2025-03-25 11:06:14 +0530

    changes

--- src/main/java/com/example/calculator/Controller/CalculatorController.java
+++ src/main/java/com/example/calculator/Controller/CalculatorController.java
@@ -1,26 +1,32 @@
 package com.example.calculator.Controller;
 
 import com.example.calculator.Services.CalculatorServices;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import com.example.calculator.model.CalculatorResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+//@AllArgsConstructor
 @RequiredArgsConstructor
 public class CalculatorController {
+
    private final CalculatorServices cs;
 
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome man";
     }
-   @GetMapping("/calculate/{num1}")
-    public CalculatorResponse calculate( @PathVariable double num1,@RequestParam double num2,
+   @GetMapping("/calculate/{id}")
+    public CalculatorResponse calculate( @PathVariable double id,@RequestParam double num2,
                                          @RequestParam String op)
     {
-
-        return cs.calculate(num1,num2,op);
+        return cs.calculate(id,num2,op);
     }
 
 }
+
+//http://localhost:8080/calculate/10?num2=5&op=add for "Pathvariable"
+//http://localhost:8080/calculate?num1=10&num2=5&op=add for RequestParam
